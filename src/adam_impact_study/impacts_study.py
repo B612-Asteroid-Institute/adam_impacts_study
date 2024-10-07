9e66999b056c1f68a419b404b6b417ff9a9b3e1e
Kathleen Kiker <kathleen@b612foundation.orgm>
2024-10-07 15:09:22 -0700

    Updating run_impact function

--- src/adam_impact_study/impacts_study.py
+++ src/adam_impact_study/impacts_study.py
@@ -12,7 +12,7 @@ from adam_impact_study.conversions import (
     od_observations_to_ades_file,
 )
 from adam_impact_study.fo_od import run_fo_od
-from adam_impact_study.sorcha_utils import run_sorcha
+from adam_impact_study.sorcha_utils import run_sorcha, write_config_file_timeframe
 
 
 class ImpactStudyResults(qv.Table):
@@ -21,52 +21,28 @@ class ImpactStudyResults(qv.Table):
     impact_probability = qv.Float64Column()
 
 
-def run_impact_study_fo(
+def run_impact_study_all(
     impactors_file: str,
-    sorcha_config_file: str,
-    sorcha_orbits_file: str,
-    sorcha_physical_params_file: str,
-    sorcha_output_file: str,
     sorcha_physical_params_string: str,
     pointing_file: str,
-    sorcha_output_name: str,
-    fo_input_file_base: str,
-    fo_output_file_base: str,
+    RUN_NAME: str,
     FO_DIR: str,
     RUN_DIR: str,
     RESULT_DIR: str,
-    chunk_size: Optional[int] = 1,
-) -> Optional[ImpactStudyResults]:
+)-> Optional[ImpactStudyResults]:
     """
-    Run an impact study using the given impactors and configuration files.
-
-    This function performs an impact probability analysis by first converting
-    impactor data into ADAM orbit objects, then running Sorcha to generate
-    observation data. The observation data is then processed using find_orb
-    and propagated to compute impact probabilities.
+    Run an impact study for all impactors in the input file.
 
     Parameters
     ----------
     impactors_file : str
         Path to the CSV file containing impactor data.
-    sorcha_config_file : str
-        Path to the Sorcha configuration file.
-    sorcha_orbits_file : str
-        Path to the file where Sorcha orbit data will be saved.
-    sorcha_physical_params_file : str
-        Path to the file where Sorcha physical parameters will be saved.
-    sorcha_output_file : str
-        Name of the Sorcha output file.
     sorcha_physical_params_string : str
-        String of space-separated physical parameters to be used in Sorcha.
+        String containing the physical parameters for the impactors.
     pointing_file : str
         Path to the file containing pointing data for Sorcha.
-    sorcha_output_name : str
-        Name for the output directory where Sorcha results will be saved.
-    fo_input_file_base : str
-        Base name for the find_orb input files.
-    fo_output_file_base : str
-        Base name for the find_orb output directories.
+    RUN_NAME : str
+        Name of the run.
     FO_DIR : str
         Directory path where the find_orb executable is located.
     RUN_DIR : str
@@ -80,20 +56,85 @@ def run_impact_study_fo(
         Table containing the results of the impact study with columns 'object_id',
         'day', and 'impact_probability'. If no impacts were found, returns None.
     """
-
     propagator = ASSISTPropagator()
-
     os.makedirs(f"{RESULT_DIR}", exist_ok=True)
 
-    # Read impactor data and convert to ADAM orbit objects
-    adam_orbit_objects = impactor_file_to_adam_orbit(impactors_file)
+    impactor_orbits = impactor_file_to_adam_orbit(impactors_file)
+
+    object_ids = impactor_orbits.object_id.unique()
+
+    impact_results = None
+
+    for obj_id in object_ids:
+        impactor_orbit = impactor_orbits[impactor_orbits.object_id == obj_id]
+        impact_result = run_impact_study_fo(
+            impactor_orbit,
+            propagator,
+            sorcha_physical_params_string,
+            pointing_file,
+            RUN_NAME,
+            FO_DIR,
+            RUN_DIR,
+            RESULT_DIR,
+        )
+
+        if impact_results is None:
+            impact_results = impact_result
+        else:
+            impact_results = qv.concatenate([impact_results, impact_result])
+
+    return impact_results
+
+
+def run_impact_study_fo(
+    impactor_orbit: qv.Table,
+    propagator: ASSISTPropagator,
+    sorcha_physical_params_string: str,
+    pointing_file: str,
+    RUN_NAME: str,
+    FO_DIR: str,
+    RUN_DIR: str,
+    RESULT_DIR: str,
+    chunk_size: Optional[int] = 1,
+) -> Optional[ImpactStudyResults]:
+    """
+    Run a single impact study using find_orb to propagate orbits and calculate impact probabilities.
+
+
+    Parameters
+    ----------
+    impactor_orbit : qv.Table
+        Table containing the initial orbits of the impactors.
+    propagator : ASSISTPropagator
+        Propagator object to propagate orbits.
+    sorcha_physical_params_string : str
+        String containing the physical parameters for the impactors.
+    pointing_file : str
+        Path to the file containing pointing data for Sorcha.
+    RUN_NAME : str
+        Name of the run.
+    FO_DIR : str
+        Directory path where the find_orb executable is located.
+    RUN_DIR : str
+        Directory path where the script is being run.
+    RESULT_DIR : str
+        Directory where the results will be stored.
+    chunk_size : int, optional
+        Number of days to propagate orbits at a time, by default 1.
+
+    Returns
+    -------
+    impact_results : ImpactStudyResults
+        Table containing the results of the impact study with columns 'object_id',
+        'day', and 'impact_probability'. If no impacts were found, returns None.
+    """
 
     # Prepare physical parameters DataFrame
     physical_params_list = [
         float(param) for param in sorcha_physical_params_string.split()
     ]
     data = []
-    for obj_id in adam_orbit_objects.object_id:
+    for obj_id in impactor_orbit.object_id:
         data.append(
             {
                 "ObjID": str(obj_id),
@@ -108,9 +149,24 @@ def run_impact_study_fo(
         )
     physical_params_df = pd.DataFrame(data)
 
+    obj_id = impactor_orbit.object_id[0]
+
+    print("Object ID: ", obj_id)
+
+    sorcha_config_file_name = f"data/sorcha_config_{RUN_NAME}_{obj_id}.ini"
+    sorcha_orbits_file = f"data/sorcha_input_{RUN_NAME}_{obj_id}.csv"
+    sorcha_physical_params_file = f"data/sorcha_params_{RUN_NAME}_{obj_id}.csv"
+    sorcha_output_name = f"sorcha_output_{RUN_NAME}_{obj_id}"
+    sorcha_output_file = f"{sorcha_output_name}.csv"
+    fo_input_file_base = f"fo_input_{RUN_NAME}_{obj_id}"
+    fo_output_file_base = f"fo_output_{RUN_NAME}_{obj_id}"
+
+    impact_date = impactor_orbit.coordinates.time.add_days(30)
+    sorcha_config_file = write_config_file_timeframe(impact_date.mjd()[0], sorcha_config_file_name)
+
     # Run Sorcha to generate observational data
     od_observations = run_sorcha(
-        adam_orbit_objects,
+        impactor_orbit,
         sorcha_config_file,
         sorcha_orbits_file,
         sorcha_physical_params_file,
@@ -123,94 +179,84 @@ def run_impact_study_fo(
     if od_observations is None:
         return None
 
-    # Iterate over each object and calculate impact probabilities
-    object_ids = od_observations.object_id.unique()
+    # Propagate the orbit and calculate the impact probability over time
     impact_results = None
-    for obj in object_ids:
-        print("Object ID: ", obj)
 
-        initial_orbit = adam_orbit_objects.apply_mask(
-            pc.equal(adam_orbit_objects.object_id, obj_id)
+    min_mjd = pc.min(od_observations.coordinates.time.mjd())
+    mask = pc.equal(od_observations.coordinates.time.mjd(), min_mjd)
+    first_obs = od_observations.apply_mask(mask).coordinates.time
+
+    # Initialize time to first observation
+    day_count = first_obs
+
+    while day_count.mjd()[0].as_py() < impact_date.mjd()[0].as_py():
+        day_count = day_count.add_days(chunk_size)
+        day = day_count.mjd()[0].as_py()
+        print("Day: ", day)
+        filtered_obs = od_observations.apply_mask(
+            pc.less_equal(od_observations.coordinates.time.days.to_numpy(), day)
         )
-        impact_date = initial_orbit.coordinates.time.add_days(30)
+        print("Filtered Observations: ", filtered_obs)
+        print("Filtered Days: ", filtered_obs.coordinates.time.days.to_numpy())
 
-        od_obs = od_observations.apply_mask(pc.equal(od_observations.object_id, obj))
+        fo_file_name = f"{fo_input_file_base}_{obj_id}_{day}.csv"
+        fo_output_folder = f"{fo_output_file_base}_{obj_id}_{day}"
+        od_observations_to_ades_file(filtered_obs, f"{RESULT_DIR}/{fo_file_name}")
 
-        min_mjd = pc.min(od_obs.coordinates.time.mjd())
-        mask = pc.equal(od_obs.coordinates.time.mjd(), min_mjd)
-        first_obs = od_obs.apply_mask(mask).coordinates.time
-
-        # initialize time to first observation
-        day_count = first_obs
-
-        while day_count.mjd()[0].as_py() < impact_date.mjd()[0].as_py():
-            day_count = day_count.add_days(chunk_size)
-            day = day_count.mjd()[0].as_py()
-            print("Day: ", day)
-            filtered_obs = od_obs.apply_mask(
-                pc.less_equal(od_obs.coordinates.time.days.to_numpy(), day)
+        # Run find_orb to compute orbits
+        try:
+            fo_orbit = run_fo_od(
+                fo_file_name,
+                fo_output_folder,
+                FO_DIR,
+                RUN_DIR,
+                RESULT_DIR,
             )
-            print("Filtered Observations: ", filtered_obs)
-            print("Filtered Days: ", filtered_obs.coordinates.time.days.to_numpy())
+        except Exception as e:
+            print(f"Error running find_orb output for {obj_id}: {e}")
+            continue
+        print(f"Fo orbit: {fo_orbit}")
+        if fo_orbit is not None:
+            print(f"Fo orbit elements: {fo_orbit.coordinates.values}")
 
-            fo_file_name = f"{fo_input_file_base}_{obj}_{day}.csv"
-            fo_output_folder = f"{fo_output_file_base}_{obj}_{day}"
-            od_observations_to_ades_file(filtered_obs, f"{RESULT_DIR}/{fo_file_name}")
-
+        if fo_orbit is not None and len(fo_orbit) > 0:
+            time = impactor_orbit.coordinates.time[0]
+            print(f"Time: {time.mjd()}")
+            orbit = fo_orbit
             try:
-                # Run find_orb to compute orbits
-                fo_orbit = run_fo_od(
-                    fo_file_name,
-                    fo_output_folder,
-                    FO_DIR,
-                    RUN_DIR,
-                    RESULT_DIR,
+                # Propagate orbits and calculate impact probabilities
+                result = propagator.propagate_orbits(
+                    orbit, time, covariance=True, num_samples=1000
                 )
+                print(f"Propagated orbit: {result}")
+                print(f"Propagated orbit elements: {result.coordinates.values}")
             except Exception as e:
-                print(f"Error running find_orb output for {obj}: {e}")
+                print(f"Error propagating orbits for {obj_id}: {e}")
                 continue
-            print(f"Fo orbit: {fo_orbit}")
-            if fo_orbit is not None:
-                print(f"Fo orbit elements: {fo_orbit.coordinates.values}")
-
-            if fo_orbit is not None and len(fo_orbit) > 0:
-                time = adam_orbit_objects.select("object_id", obj).coordinates.time[0]
-                print(f"Time: {time.mjd()}")
-                orbit = fo_orbit
-                try:
-                    # Propagate orbits and calculate impact probabilities
-                    result = propagator.propagate_orbits(
-                        orbit, time, covariance=True, num_samples=1000
-                    )
-                    print(f"Propagated orbit: {result}")
-                    print(f"Propagated orbit elements: {result.coordinates.values}")
-                except Exception as e:
-                    print(f"Error propagating orbits for {obj}: {e}")
-                    continue
-                try:
-                    results, impacts = calculate_impacts(
-                        result, 60, propagator, num_samples=10000
-                    )
-                    result.to_parquet(
-                        f"{RESULT_DIR}/propagated_orbit_{obj}_{day}.parquet"
-                    )
-                    print(f"Impacts: {impacts}")
-                    ip = calculate_impact_probabilities(results, impacts)
-                    print(f"IP: {ip.cumulative_probability[0].as_py()}")
-                except Exception as e:
-                    print(f"Error calculating impacts for {obj}: {e}")
-                    continue
-                if ip.cumulative_probability[0].as_py() is not None:
-                    impact_result = ImpactStudyResults.from_kwargs(
-                        object_id=[obj],
-                        day=[day],
-                        impact_probability=[ip.cumulative_probability[0].as_py()],
-                    )
-                    print(f"Impact Result: {impact_result}")
-                    if impact_results is None:
-                        impact_results = impact_result
-                    else:
-                        impact_results = qv.concatenate([impact_results, impact_result])
-                    print(f"Impact Results: {impact_results}")
+            try:
+                results, impacts = calculate_impacts(
+                    result, 60, propagator, num_samples=10000
+                )
+                result.to_parquet(
+                    f"{RESULT_DIR}/propagated_orbit_{obj_id}_{day}.parquet"
+                )
+                print(f"Impacts: {impacts}")
+                ip = calculate_impact_probabilities(results, impacts)
+                print(f"IP: {ip.cumulative_probability[0].as_py()}")
+            except Exception as e:
+                print(f"Error calculating impacts for {obj_id}: {e}")
+                continue
+            if ip.cumulative_probability[0].as_py() is not None:
+                impact_result = ImpactStudyResults.from_kwargs(
+                    object_id=[obj_id],
+                    day=[day],
+                    impact_probability=[ip.cumulative_probability[0].as_py()],
+                )
+                print(f"Impact Result: {impact_result}")
+                if impact_results is None:
+                    impact_results = impact_result
+                else:
+                    impact_results = qv.concatenate([impact_results, impact_result])
+                print(f"Impact Results: {impact_results}")
 
     return impact_results
