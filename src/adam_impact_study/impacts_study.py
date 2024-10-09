c5ae70e09a714b664b4d69c173db0a4aad01d71c
Kathleen Kiker <kathleen@b612foundation.orgm>
2024-10-08 23:04:11 -0700

    Quick fix

--- src/adam_impact_study/impacts_study.py
+++ src/adam_impact_study/impacts_study.py
@@ -82,6 +82,7 @@ def run_impact_study_all(
             FO_DIR,
             RUN_DIR,
             RESULT_DIR,
+            chunk_size,
         )
 
         if impact_results is None:
@@ -196,6 +197,8 @@ def run_impact_study_fo(
 
     # Initialize time to first observation
     day_count = first_obs
+
+    print ("Impact Date: ", impact_date)
 
     while day_count.mjd()[0].as_py() < impact_date.mjd()[0].as_py():
         day_count = day_count.add_days(chunk_size)
