d4c3d606503c79e943cd450dde89ad704f461d9e
Kathleen Kiker <kathleen@b612foundation.orgm>
2024-12-18 16:12:11 -0800

    Updating demo so directories can be defined on command line

--- demo/demo.py
+++ demo/demo.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 
@@ -8,11 +9,26 @@ from adam_impact_study.impacts_study import run_impact_study_all
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define the run name and directories
-RUN_NAME = "Impact_Study_Demo"
-RESULT_DIR = os.path.join(os.path.dirname(__file__), "results")
-RUN_DIR = os.getcwd()
-FO_DIR = os.path.join(os.path.dirname(__file__), "../find_orb/find_orb")
+# Set up command line arguments
+parser = argparse.ArgumentParser(description='Run impact study demo')
+parser.add_argument('--run-name', default="Impact_Study_Demo",
+                   help='Name of the impact study run (default: Impact_Study_Demo)')
+parser.add_argument('--result-dir', 
+                   default=os.path.join(os.path.dirname(__file__), "results"),
+                   help='Directory for storing results (default: ./results)')
+parser.add_argument('--run-dir', default=os.getcwd(),
+                   help='Working directory for the run (default: current working directory)')
+parser.add_argument('--fo-dir', 
+                   default=os.path.join(os.path.dirname(__file__), "find_orb/find_orb"),
+                   help='Find_Orb directory path (default: ./find_orb/find_orb)')
+
+args = parser.parse_args()
+
+# Use the command line arguments or defaults
+RUN_NAME = args.run_name
+RESULT_DIR = args.result_dir
+RUN_DIR = args.run_dir
+FO_DIR = args.fo_dir
 
 # Define the input files
 impactors_file = os.path.join(os.path.dirname(__file__), "data/10_impactors.csv")
